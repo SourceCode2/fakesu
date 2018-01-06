a33021b509f8b7d0dfeaa62523d51a0c135156b2
SourceCode2 <davidesofronia@gmail.com>
2018-01-06 11:50:42 +0100

    style copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
-/* fakeSu. Replace the "su" command, check the input and save the password entered in a file.
-   Copyright (C) 2017-2018 Davide sofronia.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  
-   Written by Davide Sofronia <davidesofronia@gmail.com>.  */
+ /********************************************************************************************
+ * fakeSu. Replace the "su" command, check the input and save the password entered in a file.*
+ * Copyright (C) 2017-2018 Davide sofronia.                                                  *
+ *                                                                                           *
+ * This program is free software: you can redistribute it and/or modify                      *
+ * it under the terms of the GNU General Public License as published by                      *
+ * the Free Software Foundation, either version 3 of the License, or                         *
+ * (at your option) any later version.                                                       *
+ *                                                                                           *
+ * This program is distributed in the hope that it will be useful,                           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                             *
+ * GNU General Public License for more details.                                              *
+ *                                                                                           *
+ * You should have received a copy of the GNU General Public License                         *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.                     *
+ * Written by Davide Sofronia <davidesofronia@gmail.com>.                                    *
+ ********************************************************************************************/
 
 #include <iostream>
 #include <cstdlib>
